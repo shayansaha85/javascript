dee644a94b5cecb189af1d2f6dbf809686ab0479
Shayan Saha <shayan851997@gmail.com>
2021-04-15 22:17:51 +0530

    js #2

---
+++ if_else.js
@@ -0,0 +1,6 @@
+var age = 19;
+if(age>=18) {
+    console.log("You're adult");
+} else {
+    console.log("You're minor");
+}
---
+++ multiple_conditions.js
@@ -0,0 +1,11 @@
+var temperature = 26;
+
+if(temperature<=10) {
+    console.log("Extreme cold");
+} else if(temperature>10 && temperature<=26) {
+    console.log("It's pleasant");
+} else if(temperature>26 && temperature<=35) {
+    console.log("Have a cold drink");
+} else {
+    console.log("Start boiling");
+}
